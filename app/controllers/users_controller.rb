cf83209304c4dd28f802440a1802ec77ca01efca
Reza <33150324+reza-nikzad@users.noreply.github.com>
2024-08-25 15:48:31 -0400

    add edit/forgot password feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!  # Ensure user is logged in
+
+  def show
+    @user = current_user  # Fetch the currently logged-in user
+  end
 end
